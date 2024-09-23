f3192f0090cf53686b716d5aea912740f32b604a
CalistoDel <calistodelaguila@gmail.com>
2024-09-23 17:04:29 +0200

    Dernière version automate

---
+++ fonctions_TP2.py
@@ -0,0 +1,32 @@
+dictionnaire ={"le" : 0, "la" : 0, "chat" : 2, "souris" : 2, "martin" : 4,
+"mange" : 3, "la" : 0, "petite" : 1, "joli" : 1, "grosse" : 1,
+"bleu" : 1, "verte" : 1, "dort" : 3,"julie" : 4, "jean" : 4, "." : 5}
+
+
+def conversion(phrase):
+#Calisto, 23/09/24, fonction qui prend en argument une phrase et la transforme en une suite de caractere selon la classe des mots la formant
+    motcorrect=''
+    sequence=[]
+    caractere_speciaux=['_','-','!','?',':',',','.']
+    phrase_decouper=phrase.split()
+    for mot in phrase_decouper:
+        for lettre in mot:
+            if lettre in caractere_speciaux:    
+                motcorrect+= lettre                 #retire les caractères spéciaux d'un mot
+                if lettre in dictionnaire:
+                    sequence.append(dictionnaire[lettre])
+    return sequence
+
+
+
+
+def automate(phrase):
+#Calisto, 23/09/24, fonction qui avec prend en argument d'entrée une phrase et qui renvoie si elle est corect syntaxiquement ou non
+    sequence=conversion(phrase)
+    etat=0                      #initialisation de l'état
+    matriceEE=[[1,8,8,5,8,8,8,8,8,8],[8,1,2,8,8,5,6,8,8,8],[8,2,8,8,8,6,8,8,8,8],[8,8,3,8,3,8,8,8,8,8],[4,8,8,7,8,8,8,8,8,8],[8,8,8,9,8,8,9,9,8,9]]
+    matriceES=[0,0,0,1,0,0,1,1,0,1]
+    for k in sequence:
+        etat=matriceEE[k][etat]
+    s=matriceES[etat]
+    print(s)
---
+++ main_TP12.py
@@ -0,0 +1,5 @@
+from fonctions_TP2 import conversion
+from fonctions_TP2 import automate
+
+
+automate("le joli chat mange")
